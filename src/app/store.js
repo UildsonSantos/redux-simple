b606220ece607087c408e46fbec66ebbabe37360
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-10-30 16:31:00 -0300

    Connect userRegistry reducer to Store interface

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import userRegistryReducer from '../features/UserRegistry/userRegistrySlice';
 
 export const store = configureStore({
   reducer: {
+    'userRegistry': userRegistryReducer
   },
 });
