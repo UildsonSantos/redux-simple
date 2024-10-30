c9916af12f8a6d912495647996d4bbbaf752fff7
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-10-30 16:09:25 -0300

    Update component to utilize state hooks for user list management and add dynamic user addition

--- src/features/UserRegistry/UserRegistry.js
+++ src/features/UserRegistry/UserRegistry.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function UserRegistry() {
+    const [new_user, setNew_user] = useState('');
+    const [users, setUsers] = useState([]);
+
+    function add(){
+        if (new_user !== '')     
+            setUsers([new_user, ...users]);
+        setNew_user('');
+    }
     return (
         <div className="user">
             <h2>Registro de usuários</h2>
-            <input type="text" value=""/>
-            <button>Adicionar...</button>
+            <input type="text" value={new_user} onChange={(ev) => setNew_user(ev.target.value)}/>
+            <button onClick={()=> add()}>Adicionar...</button>
             <ul>
-                <li>Lia</li>
-                <li>Juliana</li>
-                <li>Carmelinda</li>
+                {users.map((user, index) => <li key={index} >{user}</li>)}
             </ul>
         </div>
     );
